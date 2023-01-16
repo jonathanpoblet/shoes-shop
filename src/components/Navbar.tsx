20419edcdda775c083ba907cc26e971f01c7c456
Jonathan Poblet <jonathanpoblet228@gmail.com>
2023-01-16 13:05:01 -0300

    prototype finished

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,42 @@
 import React from 'react';
 import { Container,Nav,Button, Navbar as NavbarBS } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { useShoppingCart } from '../context/ShoppingCartContext';
 
 export function Navbar() {
-  return (
-    <NavbarBS sticky='top' className='bg-white shadow-sm mb-3'>
-        <Container>
-            <Nav className='me-auto'>
-                <Nav.Link to='/' as={ NavLink }>
-                    Home
-                </Nav.Link>
-                <Nav.Link to='/store' as={ NavLink }>
-                    Store
-                </Nav.Link>
-                <Nav.Link to='/about' as={ NavLink }>
-                    About
-                </Nav.Link>
-            </Nav>
-            <Button 
-                style={{width: '4rem' , height: '3rem'}} 
-                variant='outline-primary'>
-                Cart
-            </Button>
-            <div 
-                className='rounded-circle bg-danger d-flex justify-content-center align-items-center'
-                style={{width:'30px'}}>
-                3
-            </div>
-        </Container>
-    </NavbarBS>
+
+    const { openCart, cartQuantity } = useShoppingCart();
+
+    return (
+        <NavbarBS sticky='top' className='bg-white shadow-sm mb-3'>
+            <Container>
+                <Nav className='me-auto'>
+                    <Nav.Link to='/' as={ NavLink }>
+                        Home
+                    </Nav.Link>
+                    <Nav.Link to='/store' as={ NavLink }>
+                        Store
+                    </Nav.Link>
+                    <Nav.Link to='/about' as={ NavLink }>
+                        About
+                    </Nav.Link>
+                </Nav>
+                <Button 
+                    onClick={openCart}
+                    style={{width: '4rem' , height: '3rem'}} 
+                    variant='outline-primary'>
+                    Cart
+                </Button>
+                {
+                    cartQuantity > 0 && (
+                        <div 
+                            className='rounded-circle bg-danger d-flex justify-content-center align-items-center'
+                            style={{width:'30px'}}>
+                            { cartQuantity }
+                        </div>
+                    )
+                }
+            </Container>
+        </NavbarBS>
   )
 }
--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
+import { useShoppingCart } from '../context/ShoppingCartContext';
 import { formatCurrency } from '../utils/formatCurrency';
 
 type StoreItemProps = {
@@ -10,7 +11,8 @@ type StoreItemProps = {
 }
 
 export default function StoreItem({id,name,price,imgUrl}: StoreItemProps) {
-    const quantity = 1
+    const { getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart } = useShoppingCart();
+    const quantity = getItemQuantity(id);
     return (
         <Card className='h-100'>
             <Card.Img variant='top' src={ imgUrl } height='200px' style={{ objectFit: 'cover'}} />
@@ -22,19 +24,19 @@ export default function StoreItem({id,name,price,imgUrl}: StoreItemProps) {
                 <div className='mt-auto'>
                     {
                         quantity === 0 ? (
-                            <Button className='w-100'> Add To Cart</Button>
+                            <Button className='w-100' onClick={() => increaseCartQuantity(id)}> Add To Cart</Button>
                         ) 
                         :
                         <div className='d-flex align-items-center flex-column' style={{gap: '.5rem'}}>
                             <div className='d-flex align-items-center justify-content-center' style={{gap: '.5rem'}}>
-                                <Button>-</Button>
+                                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
                                 <div>
                                     <span className='fs-3'>{ quantity }</span>
                                     in cart
                                 </div>
-                                <Button>+</Button>
+                                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
                             </div>
-                            <Button variant='danger' size='small'>Remove</Button>
+                            <Button variant='danger' size='sm' onClick={() => removeFromCart(id)}>Remove</Button>
                         </div>
                     }
                 </div>
